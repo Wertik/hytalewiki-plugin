190372f98ab2da71d3c4e429a2497472fa2584ea
Wertik <jregvx@gmail.com>
2026-01-23 16:04:54 +0100

    ref: remove experimental ui stuff (moved to sep branch)

--- src/main/java/org/hytalewiki/WikiCommand.java
+++ src/main/java/org/hytalewiki/WikiCommand.java
@@ -1,25 +1,15 @@
 package org.hytalewiki;
 
-import au.ellie.hyui.builders.ContainerBuilder;
-import au.ellie.hyui.builders.PageBuilder;
-import au.ellie.hyui.builders.UIElementBuilder;
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.asset.type.item.config.Item;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.CommandSender;
-import com.hypixel.hytale.server.core.command.system.arguments.system.FlagArg;
 import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
 import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.world.World;
-import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 import org.hytalewiki.net.RequestException;
 import org.hytalewiki.net.WikiClient;
@@ -31,7 +21,6 @@ import javax.annotation.Nonnull;
 import java.awt.*;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.logging.Level;
 
 public class WikiCommand extends AbstractAsyncCommand {
 
@@ -41,17 +30,11 @@ public class WikiCommand extends AbstractAsyncCommand {
 
     private final WikiClient client = new WikiClient(HytaleWikiPlugin.HYTALE_WIKI_ORG_BASE_URL);
 
-    private final WikiTextGenerator generator = new WikiTextGenerator();
-
-    public WikiTextGenerator getGenerator() {
-        return generator;
-    }
-
     public WikiCommand(HytaleWikiPlugin plugin) {
         super("wiki", "Opens a wiki link for the specified item.");
         this.plugin = plugin;
 
-        this.setPermissionGroup(GameMode.Adventure); // Allows the command to be used by anyone, not just OP
+        this.setPermissionGroup(GameMode.Adventure);
 
         this.addSubCommand(new WikiPageCommand(this));
         this.addSubCommand(new WikiHandCommand(this));
@@ -59,22 +42,16 @@ public class WikiCommand extends AbstractAsyncCommand {
     }
 
     private static class WikiHandCommand extends AbstractAsyncCommand {
-        private final FlagArg uiFlag;
-
         private final WikiCommand parent;
 
         WikiHandCommand(WikiCommand command) {
             super("hand", "Provide a link to the wiki page for the item in hand.");
             this.parent = command;
-
-            this.uiFlag = this.withFlagArg("ui", "Open the in-game UI.");
         }
 
         @NonNullDecl
         @Override
         protected CompletableFuture<Void> executeAsync(@NonNullDecl CommandContext context) {
-            boolean ui = this.uiFlag.get(context);
-
             if (!(context.sender() instanceof Player player)) {
                 context.sendMessage(Message.raw("Only players can do this.").color(Color.RED));
                 return CompletableFuture.completedFuture(null);
@@ -105,10 +82,6 @@ public class WikiCommand extends AbstractAsyncCommand {
                 return CompletableFuture.completedFuture(null);
             }
 
-            if (ui) {
-                return CompletableFuture.runAsync(() -> this.parent.openPageUI(player, page));
-            }
-
             context.sendMessage(this.parent.makeResultRow(page.getTitle(), page.getKey()));
             return CompletableFuture.completedFuture(null);
         }
@@ -119,8 +92,6 @@ public class WikiCommand extends AbstractAsyncCommand {
         @Nonnull
         private final RequiredArg<String> termArg;
 
-        private final FlagArg uiFlag;
-
         private final WikiCommand parent;
 
         WikiSearchCommand(WikiCommand command) {
@@ -128,7 +99,6 @@ public class WikiCommand extends AbstractAsyncCommand {
             this.parent = command;
 
             this.termArg = this.withRequiredArg("term", "Term to search for.", ArgTypes.STRING);
-            this.uiFlag = this.withFlagArg("ui", "Open the in-game UI.");
         }
 
         @NonNullDecl
@@ -139,8 +109,6 @@ public class WikiCommand extends AbstractAsyncCommand {
             String term = context.get(this.termArg)
                     // String extra quotes when using "Hello world" syntax for string arguments
                     .replace("\"", "");
-
-            boolean ui = context.get(this.uiFlag);
 
             // todo: try and look for an item with the term as an id, search for the display name
             // todo: if there's an in-game item with the name, invite the user to create it
@@ -154,18 +122,6 @@ public class WikiCommand extends AbstractAsyncCommand {
 
             final SearchEntry exactMatch = parent.findExactMatch(result, term);
 
-            if (exactMatch != null && ui && context.sender() instanceof Player player) {
-
-                PageObject page;
-                try {
-                    page = this.parent.client.page(exactMatch.getKey());
-                } catch (RequestException e) {
-                    return CompletableFuture.failedFuture(e);
-                }
-
-                return CompletableFuture.runAsync(() -> parent.openPageUI(player, page));
-            }
-
             Message message = parent.makeHeader("Results");
             if (exactMatch == null) {
                 message.insert(this.parent.makeCreateNotice(term)).insert(Message.raw("\n"));
@@ -178,7 +134,6 @@ public class WikiCommand extends AbstractAsyncCommand {
     }
 
     private class WikiPageCommand extends AbstractAsyncCommand {
-        private final FlagArg uiFlag;
         private final RequiredArg<String> keyArg;
 
         private final WikiCommand parent;
@@ -189,7 +144,6 @@ public class WikiCommand extends AbstractAsyncCommand {
 
             this.setPermissionGroup(GameMode.Adventure);
 
-            this.uiFlag = this.withFlagArg("ui", "Open UI with the page rendered.");
             this.keyArg = this.withRequiredArg("key", "The key to search for.", ArgTypes.STRING);
         }
 
@@ -199,17 +153,6 @@ public class WikiCommand extends AbstractAsyncCommand {
             String key = context.get(this.keyArg)
                     // String extra quotes when using "Hello world" syntax for string arguments
                     .replace("\"", "");
-
-            log.at(Level.INFO).log("Key: '" + key + "'");
-
-            boolean ui = this.uiFlag.get(context);
-
-            CommandSender sender = context.sender();
-
-            if (ui && !(sender instanceof Player)) {
-                context.sendMessage(Message.raw("Cannot open UI for console.").color(Color.RED));
-                return CompletableFuture.completedFuture(null);
-            }
 
             PageObject page;
             try {
@@ -221,10 +164,6 @@ public class WikiCommand extends AbstractAsyncCommand {
             if (page == null || page.getKey() == null) {
                 context.sendMessage(makeCreateNotice(key));
                 return CompletableFuture.completedFuture(null);
-            }
-
-            if (ui) {
-                return CompletableFuture.runAsync(() -> openPageUI((Player) sender, page));
             }
 
             context.sendMessage(this.parent.makeResultRow(page.getTitle(), page.getKey()));
@@ -244,41 +183,6 @@ public class WikiCommand extends AbstractAsyncCommand {
         ));
 
         return CompletableFuture.completedFuture(null);
-    }
-
-    private void openPageUI(Player player, PageObject page) {
-        player.sendMessage(Message.raw("UIs are highly unstable. Expect it not to work.").color(Color.lightGray));
-
-        World world = player.getWorld();
-
-        if (world == null) {
-            player.sendMessage(Message.raw("Failed to open UI.").color(Color.RED));
-            log.at(Level.SEVERE).log("Failed to open UI for player " + player.getDisplayName() + " (world == null)");
-            return;
-        }
-
-        world.execute(() -> {
-            Ref<EntityStore> ref = player.getReference();
-
-            if (ref == null) {
-                player.sendMessage(Message.raw("Failed to open UI.").color(Color.RED));
-                log.at(Level.SEVERE).log("Failed to open UI for player " + player.getDisplayName() + " (ref == null)");
-                return;
-            }
-
-            Store<EntityStore> store = ref.getStore();
-
-            PlayerRef playerRefComponent = store.getComponent(ref, PlayerRef.getComponentType());
-
-            UIElementBuilder<?> root = getGenerator().generate(page.getSource());
-
-            ContainerBuilder container = ContainerBuilder.container().addContentChild(root);
-            container.withTitleText(page.getTitle());
-
-            PageBuilder pageBuilder = PageBuilder.pageForPlayer(playerRefComponent);
-            pageBuilder.addElement(container);
-            pageBuilder.open(store);
-        });
     }
 
     private SearchEntry findExactMatch(SearchResult result, String term) {
